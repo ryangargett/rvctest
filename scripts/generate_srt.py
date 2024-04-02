624867d0d424eb9b0212325472f764ccbf681afc
Ryan Gargett <r.d.gargett@gmail.com>
2024-04-02 13:29:08 +0800

    whisperx conversion for faster asr

--- scripts/generate_srt.py
+++ scripts/generate_srt.py
@@ -1,20 +1,27 @@
 import os
 from nltk import sent_tokenize
-import whisper
+import whisperx
 import pysrt
 from pydub import AudioSegment
 
 WORDS_PER_MINUTE = 150
-ASR_MODEL_ARCH = "base"
+ASR_MODEL_ARCH = "large-v2"
 
 def process_audio(path):
     
-    asr_model = whisper.load_model(ASR_MODEL_ARCH)
-    transcription = asr_model.transcribe(path)
+    asr_model = whisperx.load_model(ASR_MODEL_ARCH, device = "cuda", compute_type = "float16")
     
-    sentences = sent_tokenize(transcription["text"])
+    audio = whisperx.load_audio(path)
+    transcription = asr_model.transcribe(audio, batch_size=16)
     
-    segment_audio(path, sentences)
+    alg_model, metadata = whisperx.load_align_model(language_code=transcription["language"], device="cuda")
+    aligned_transcription = whisperx.align(transcription["segments"], alg_model, metadata, audio, device="cuda", return_char_alignments=False)
+    
+    print(aligned_transcription["segments"])
+    
+    
+    
+    #segment_audio(path, sentences)
     
     
 def segment_audio(path, sentences):
