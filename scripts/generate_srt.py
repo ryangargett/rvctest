97421dfd4edd17496efd3058f1667070dccf89b0
Ryan Gargett <r.d.gargett@gmail.com>
2024-04-03 12:15:21 +0800

    fixed error in training dir walk

--- scripts/generate_srt.py
+++ scripts/generate_srt.py
@@ -3,34 +3,30 @@ from nltk import sent_tokenize
 import whisperx
 import pysrt
 from pydub import AudioSegment
-import json
 
-WORDS_PER_MINUTE = 150
 ASR_MODEL_ARCH = "large-v2"
 DEF_BASE_NAME = "segments"
 
 def process_audio(path, process_batch = False, buffer = 0):
     
-    base_name = setup_training_dir(path, process_batch)
+    setup_training_dir(path, process_batch)
     
-    if process_batch:
-        audio_files = [os.path.join(path, file) for file in os.listdir(path) if file.endswith(".wav")]
-        combined_transcription = {"segments": []}
-        
-        for audio_file in audio_files:
+    start_idx = 0
+    base_name = DEF_BASE_NAME
+    
+    for audio_file in sorted(os.listdir(path)):
+        if audio_file.endswith(".wav"):
+            audio_file = os.path.join(path, audio_file)
+            print(f"processing {audio_file}")
             sentences = transcribe_audio(audio_file)
-            combined_transcription["segments"].extend(sentences["segments"])
-
-            segment_audio(audio_file, sentences, base_name, buffer)
-        
-    else:
-        
-        for audio_file in os.listdir(path):
-            if audio_file.endswith(".wav"):
-                audio_path = os.path.join(path, audio_file)
-                sentences = transcribe_audio(audio_path)
-                segment_audio(audio_path, sentences, base_name, buffer)
-      
+            
+            if process_batch is False:
+                base_name = os.path.splitext(os.path.basename(audio_file))[0]
+            
+            segment_audio(audio_file, sentences, base_name, buffer = 0, start_idx = start_idx)
+            
+            if process_batch is True:
+                start_idx = len(sentences["segments"]) + 1
 
 def transcribe_audio(path):
     
@@ -61,11 +57,9 @@ def setup_training_dir(path, process_batch):
         base_name = DEF_BASE_NAME
         os.makedirs(f"data/{base_name}", exist_ok=True)
         print(f"initialized training directory: data/{base_name}")
-        
-    return base_name
     
     
-def segment_audio(path, sentences, base_name, buffer):
+def segment_audio(path, sentences, base_name, buffer, start_idx):
     
     audio = AudioSegment.from_file(path)
     srt = pysrt.SubRipFile()
@@ -81,10 +75,10 @@ def segment_audio(path, sentences, base_name, buffer):
         srt.append(subtitle)
         segment = audio[start_time:end_time]
         
-        segment.export(os.path.join(f"data/{base_name}", f"segment_{idx}.wav"), format="wav")
+        segment.export(os.path.join(f"data/{base_name}", f"segment_{idx + start_idx}.wav"), format="wav")
         
         start_time = end_time
-    srt_name = f"data/{base_name}.srt"
+    srt_name = f"data/{os.path.splitext(os.path.basename(path))[0]}.srt"
     srt.save(srt_name, encoding = "utf-8")
     
     
