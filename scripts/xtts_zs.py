22051ebce274e28bd36d56635b7228dfe66adf39
Ryan Gargett <r.d.gargett@gmail.com>
2024-04-11 14:39:00 +0800

    constructed basic test harness for zero shot model

--- scripts/xtts_zs.py
+++ scripts/xtts_zs.py
@@ -4,8 +4,8 @@
 # Created Date: Sun Apr 07 2024                   #
 # Author: Ryan Gargett                            #
 # -----                                           #
-# Last Modified: Sun Apr 07 2024                  #
-# Modified By: Ryan Gargett                       #
+#Last Modified: Thu Apr 11 2024                  #
+#Modified By: Ryan Gargett                       #
 ##################################################
 
 
@@ -17,41 +17,96 @@ from TTS.tts.models.xtts import Xtts
 
 import time
 
+STYLE_REFERENCE = {
+    "amusement": ["georgia_ds/amuse_1.wav"],
+    "compassion": ["compassion_1.wav", "compassion_2.wav"],
+    "concern": ["concern_1.wav"],
+    "curiosity": ["curiosity_1.wav", "curiosity_2.wav"],
+    "empathy": ["empathy_1.wav", "empathy_2.wav", "empathy_3.wav"],
+    "excitement": ["excite_1.wav", "excite_2.wav"],
+    "happiness": ["georgia_ds/happy_2.wav", "georgia_ds/happy_3.wav"],
+    "neutral": ["neutral_1.wav"],
+    "pride": ["pride_1.wav", "pride_2.wav"],
+    "supportive": ["support_1.wav", "support_2.wav"],
+    "surprise": ["surprise_1.wav", "surprise_2.wav"]
+}
+
 # Add here the xtts_config path
 CONFIG_PATH = "/home/ryan/projects/RVCTest/out/GPT_XTTS_v2.0_GEORGIA_FT-April-06-2024_11+50PM-d6edb46/config.json"
 # Add here the vocab file that you have used to train the model
 TOKENIZER_PATH = "/home/ryan/projects/RVCTest/out/XTTS_v2.0_original_model_files/vocab.json"
 # Add here the checkpoint that you want to do inference with
-XTTS_CHECKPOINT = "/home/ryan/projects/RVCTest/out/GPT_XTTS_v2.0_GEORGIA_FT-April-06-2024_11+50PM-d6edb46/best_model.pth"
-# Add here the speaker reference
-SPEAKER_REFERENCE = ["/home/ryan/projects/RVCTest/georgia_ds/Georgia - Pride - v2.post.wav"]
+XTTS_CHECKPOINT = "/home/ryan/projects/RVCTest/out/GPT_XTTS_v2.0_GEORGIA_FT-April-06-2024_11+50PM-d6edb46/best_model_2610.pth"
 
-# output wav path
-OUTPUT_WAV_PATH = "xtts-ft.wav"
 
-print("Loading model...")
-config = XttsConfig()
-config.load_json(CONFIG_PATH)
-model = Xtts.init_from_config(config)
-model.load_checkpoint(config, checkpoint_path=XTTS_CHECKPOINT, vocab_path=TOKENIZER_PATH, use_deepspeed=False)
-model.cuda()
+def _get_model_config(config_path):
+    config = XttsConfig()
+    config.load_json(config_path)
 
-print("Computing speaker latents...")
-gpt_cond_latent, speaker_embedding = model.get_conditioning_latents(audio_path=SPEAKER_REFERENCE)
+    return config
 
-print("Inference...")
 
-start_time = time.perf_counter()
+def load_model(model_path, config_path, tokenizer_path):
 
-out = model.inference(
-    " You know, our team really is something else. We've accomplished feats that others could only dream of!",
-    "en",
-    gpt_cond_latent,
-    speaker_embedding,
-    temperature=0.6,  # Add custom parameters here
-)
+    print(f"Loading model config....")
+    config = _get_model_config(config_path)
 
-end_time = time.perf_counter()
+    print(f"Loading model....")
+    model = Xtts.init_from_config(config)
+    model.load_checkpoint(config, checkpoint_path=model_path, vocab_path=tokenizer_path, use_deepspeed=False)
+    model.cuda()
 
-print(f"Elapsed time: {end_time - start_time} seconds")
-torchaudio.save(OUTPUT_WAV_PATH, torch.tensor(out["wav"]).unsqueeze(0), 24000)
+    return model
+
+
+def generate_style(model, style):
+
+    if style not in STYLE_REFERENCE:
+        raise ValueError(f"Style '{style}' not found in reference lookup.")
+
+    reference = STYLE_REFERENCE[style]
+
+    gpt_cond_latent, speaker_embedding = model.get_conditioning_latents(audio_path=reference)
+    return gpt_cond_latent, speaker_embedding
+
+
+def inference(text, model, gpt_cond_latent, speaker_embedding, track_performance=False, output_path="output.wav"):
+
+    start_time = time.perf_counter()
+
+    out = model.inference(
+        text,
+        "en",
+        gpt_cond_latent,
+        speaker_embedding,
+        temperature=0.65
+    )
+
+    end_time = time.perf_counter()
+
+    if track_performance:
+        print(f"Elapsed time: {end_time - start_time} seconds")
+
+    torchaudio.save(output_path, torch.tensor(out["wav"]).unsqueeze(0), 24000)
+
+
+def main():
+
+    style_name = "happiness"
+
+    model = load_model(XTTS_CHECKPOINT, CONFIG_PATH, TOKENIZER_PATH)
+    gpt_cond_latent, speaker_embedding = generate_style(model, style_name)
+
+    # model performs significantly better with emotionally aligned sentence context
+    text = f"Hello! This is a test generation, attempting to capture {style_name} style."
+
+    num_tests = 5
+
+    for test in range(num_tests):
+        print(f"Running test {test+1} of {num_tests}")
+        inference(text, model, gpt_cond_latent, speaker_embedding,
+                  track_performance=True, output_path=f"output_{test}.wav")
+
+
+if __name__ == "__main__":
+    main()
